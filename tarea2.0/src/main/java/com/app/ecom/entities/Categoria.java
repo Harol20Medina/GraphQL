3fcb3f1af8e433c1ed3459d9bfa5876123cd67f3
Harol20Medina <benja15mz@gmail.com>
2023-04-30 19:01:04 -0500

    descripcion del commit

---
+++ tarea2.0/src/main/java/com/app/ecom/entities/Categoria.java
@@ -0,0 +1,47 @@
+package com.app.ecom.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Categoria {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
+
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
